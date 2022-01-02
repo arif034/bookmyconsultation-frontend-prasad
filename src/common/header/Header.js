f349a1e788fa6e7b4aff7a2d7e21f1813d0a3c99
Prasad Sawant <prasad.sawant20397@gmail.com>
2022-01-02 16:41:14 +0530

    logo fixed

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Header.css";
 import logo from "../../assets/logo.jpeg";
 import Modal from "react-modal";
-import { Button, Tabs, Tab, Typography } from "@material-ui/core";
+import { Button, Tabs, Tab } from "@material-ui/core";
 import TabContainer from "../tabContainer/TabContainer";
 import Login from "../../screens/login/Login";
 import Register from "../../screens/register/Register";
